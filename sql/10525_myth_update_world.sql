70e7cf2eae43604a37aaab9374935a19bb6136cd
vertrigo <admin@uncover-wow.ru>
2012-06-16 22:59:19 +0300

    Set parametr of encoding (for normal encoding on linux)

--- sql/10525_myth_update_world.sql
+++ sql/10525_myth_update_world.sql
@@ -1,3 +1,4 @@
+SET NAMES 'utf8';
 DELETE FROM `trinity_string` WHERE `entry` IN ('787','788','789','790');
 INSERT INTO `trinity_string` (`entry`,`content_default`,`content_loc8`) VALUES
 ('788','|CFF00CCFF<Mod>|cffff0000[%s]|c1f40af20 announce: |cffffff00%s|r','|CFF00CCFF<Модератор>|cffff0000[%s]|c1f40af20 анонс: |cffffff00%s|r'),
